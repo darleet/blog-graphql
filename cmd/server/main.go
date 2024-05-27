be029f3e82e69b0dea4ed5ffe5184b62cf2cce41
darleet <darleet@mail.ru>
2024-05-28 00:00:34 +0300

    Added entry point

---
+++ cmd/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/darleet/blog-graphql/internal/ports/gql/resolver"
+	"github.com/darleet/blog-graphql/internal/ports/gql/runtime"
+	"github.com/darleet/blog-graphql/internal/repository/local"
+	"github.com/darleet/blog-graphql/internal/usecase/article"
+	"github.com/darleet/blog-graphql/internal/usecase/comment"
+	"github.com/darleet/blog-graphql/internal/usecase/vote"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8888"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	repo := local.NewRepository()
+
+	articles := article.NewUsecase(repo)
+	comments := comment.NewUsecase(nil)
+	votes := vote.NewUsecase(nil)
+
+	res := resolver.NewRootResolvers(articles, comments, nil, votes)
+	srv := handler.NewDefaultServer(runtime.NewExecutableSchema(res))
+
+	srv.AddTransport(&transport.Websocket{})
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
