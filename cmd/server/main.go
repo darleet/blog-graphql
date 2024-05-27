a294ec7b4fda640f7bf31f9db25a6227532baf11
darleet <darleet@mail.ru>
2024-05-28 00:51:51 +0300

    Fixed middleware import in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/darleet/blog-graphql/internal/middleware/auth"
 	"github.com/darleet/blog-graphql/internal/ports/gql/resolver"
 	"github.com/darleet/blog-graphql/internal/ports/gql/runtime"
 	"github.com/darleet/blog-graphql/internal/repository/local"
@@ -35,7 +36,7 @@ func main() {
 	srv.AddTransport(&transport.Websocket{})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", auth.Middleware(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
