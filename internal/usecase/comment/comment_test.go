4c67bee5e1b941646690e8cb19b381f5d942e573
darleet <darleet@mail.ru>
2024-05-27 21:06:41 +0300

    Added tests for Comment Usecase

---
+++ internal/usecase/comment/comment_test.go
@@ -0,0 +1,133 @@
+package comment
+
+import (
+	"context"
+	"github.com/darleet/blog-graphql/internal/model"
+	"github.com/darleet/blog-graphql/internal/usecase/comment/mocks"
+	"github.com/darleet/blog-graphql/pkg/errors"
+	"github.com/darleet/blog-graphql/pkg/utils"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+var comment = &model.Comment{
+	ID:        "1234",
+	Content:   "Some test comment",
+	UserID:    "4321",
+	CreatedAt: time.Now(),
+}
+
+type CommentTestSuite struct {
+	suite.Suite
+	repo *mocks.Repository
+	uc   *Usecase
+}
+
+func TestArticleTestSuite(t *testing.T) {
+	suite.Run(t, new(CommentTestSuite))
+}
+
+func (s *CommentTestSuite) SetupSuite() {
+	s.repo = mocks.NewRepository(s.T())
+	s.uc = NewUsecase(s.repo)
+}
+
+func (s *CommentTestSuite) TestCreate() {
+	input := model.NewComment{
+		Content:   comment.Content,
+		ArticleID: "1234",
+	}
+
+	s.repo.On("Create", mock.Anything,
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("model.NewComment")).
+		Return(comment, nil).
+		Once()
+
+	ctx := utils.SetUserID(context.Background(), "4321")
+	got, err := s.uc.Create(ctx, input)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), comment, got)
+}
+
+func (s *CommentTestSuite) TestCreateNoID() {
+	input := model.NewComment{
+		Content:   comment.Content,
+		ArticleID: "1234",
+	}
+
+	got, err := s.uc.Create(context.Background(), input)
+	require.ErrorIs(s.T(), err, errors.Unauthorized)
+	require.Nil(s.T(), got)
+}
+
+func (s *CommentTestSuite) TestUpdate() {
+	input := model.UpdateComment{
+		ID:      "1234",
+		Content: &comment.Content,
+	}
+
+	s.repo.On("GetAuthorID", mock.Anything,
+		mock.AnythingOfType("string")).
+		Return("4321", nil).
+		Once()
+
+	s.repo.On("Update", mock.Anything,
+		mock.AnythingOfType("model.UpdateComment")).
+		Return(comment, nil).
+		Once()
+
+	ctx := utils.SetUserID(context.Background(), "4321")
+	got, err := s.uc.Update(ctx, input)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), comment, got)
+}
+
+func (s *CommentTestSuite) TestUpdateWrongAuthor() {
+	input := model.UpdateComment{
+		ID:      "1234",
+		Content: &comment.Content,
+	}
+
+	s.repo.On("GetAuthorID", mock.Anything,
+		mock.AnythingOfType("string")).
+		Return("1337", nil).
+		Once()
+
+	ctx := utils.SetUserID(context.Background(), "4321")
+	got, err := s.uc.Update(ctx, input)
+	require.ErrorIs(s.T(), err, errors.Forbidden)
+	require.Nil(s.T(), got)
+}
+
+func (s *CommentTestSuite) TestDelete() {
+	s.repo.On("GetAuthorID", mock.Anything,
+		mock.AnythingOfType("string")).
+		Return("4321", nil).
+		Once()
+
+	s.repo.On("Delete", mock.Anything,
+		mock.AnythingOfType("string")).
+		Return(true, nil).
+		Once()
+
+	ctx := utils.SetUserID(context.Background(), "4321")
+	ok, err := s.uc.Delete(ctx, "1234")
+	require.NoError(s.T(), err)
+	require.True(s.T(), ok)
+}
+
+func (s *CommentTestSuite) TestDeleteWrongAuthor() {
+	s.repo.On("GetAuthorID", mock.Anything,
+		mock.AnythingOfType("string")).
+		Return("1337", nil).
+		Once()
+
+	ctx := utils.SetUserID(context.Background(), "4321")
+	ok, err := s.uc.Delete(ctx, "1234")
+	require.ErrorIs(s.T(), err, errors.Forbidden)
+	require.False(s.T(), ok)
+}
