43ff3b7b772b86381b2f5900e472e84ff840a1c1
darleet <darleet@mail.ru>
2024-05-27 14:42:15 +0300

    Created article usecase

---
+++ internal/usecase/article/article.go
@@ -0,0 +1,77 @@
+package article
+
+import (
+	"context"
+
+	"github.com/darleet/blog-graphql/internal/model"
+	"github.com/darleet/blog-graphql/pkg/errors"
+	"github.com/darleet/blog-graphql/pkg/utils"
+)
+
+type Repository interface {
+	Create(ctx context.Context, userID string, input model.NewArticle) (*model.Article, error)
+	Update(ctx context.Context, input model.UpdateArticle) (*model.Article, error)
+	Delete(ctx context.Context, id string) (bool, error)
+	GetList(ctx context.Context, after *string, sort *model.Sort) ([]*model.Article, error)
+	GetByID(ctx context.Context, articleID string) (*model.Article, error)
+}
+
+type Usecase struct {
+	repo Repository
+}
+
+func NewUsecase(repo Repository) *Usecase {
+	return &Usecase{
+		repo: repo,
+	}
+}
+
+func (a *Usecase) IsAuthor(ctx context.Context, articleID string) (bool, error) {
+	userID, err := utils.GetUserID(ctx)
+	if err != nil {
+		return false, errors.NewUnauthorizedError(err)
+	}
+	article, err := a.repo.GetByID(ctx, articleID)
+	if err != nil {
+		return false, err
+	}
+	return userID == article.Author.ID, nil
+}
+
+func (a *Usecase) Create(ctx context.Context, input model.NewArticle) (*model.Article, error) {
+	userID, err := utils.GetUserID(ctx)
+	if err != nil {
+		return nil, errors.NewUnauthorizedError(err)
+	}
+	return a.repo.Create(ctx, userID, input)
+}
+
+func (a *Usecase) Update(ctx context.Context, input model.UpdateArticle) (*model.Article, error) {
+	isAuthor, err := a.IsAuthor(ctx, input.ID)
+	if err != nil {
+		return nil, err
+	}
+	if !isAuthor {
+		return nil, errors.NewForbiddenError("you are not the author of this article")
+	}
+	return a.repo.Update(ctx, input)
+}
+
+func (a *Usecase) Delete(ctx context.Context, id string) (bool, error) {
+	isAuthor, err := a.IsAuthor(ctx, id)
+	if err != nil {
+		return false, err
+	}
+	if !isAuthor {
+		return false, errors.NewForbiddenError("you are not the author of this article")
+	}
+	return a.repo.Delete(ctx, id)
+}
+
+func (a *Usecase) GetList(ctx context.Context, after *string, sort *model.Sort) ([]*model.Article, error) {
+	return a.repo.GetList(ctx, after, sort)
+}
+
+func (a *Usecase) GetByID(ctx context.Context, articleID string) (*model.Article, error) {
+	return a.repo.GetByID(ctx, articleID)
+}
