575aed18d1164ebdb164b58f2dac8bd75fb909ab
darleet <darleet@mail.ru>
2024-05-27 14:41:56 +0300

    Created err pkg

---
+++ pkg/errors/errors.go
@@ -0,0 +1,96 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var (
+	BadRequest          = errors.New("bad request")
+	NotFound            = errors.New("not found")
+	Conflict            = errors.New("conflict")
+	Forbidden           = errors.New("forbidden")
+	Unauthorized        = errors.New("unauthorized")
+	InternalServerError = errors.New("internal server error")
+)
+
+type RestErr interface {
+	Status() int
+	Error() string
+	Causes() interface{}
+}
+
+type RestError struct {
+	ErrStatus int         `json:"status,omitempty"`
+	ErrError  string      `json:"error,omitempty"`
+	ErrCauses interface{} `json:"-"`
+}
+
+func (e RestError) Error() string {
+	return fmt.Sprintf("status: %d - errors: %s - causes: %v", e.ErrStatus, e.ErrError, e.ErrCauses)
+}
+
+func (e RestError) Status() int {
+	return e.ErrStatus
+}
+
+func (e RestError) Causes() interface{} {
+	return e.ErrCauses
+}
+
+func NewRestError(status int, err string, causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: status,
+		ErrError:  err,
+		ErrCauses: causes,
+	}
+}
+
+func NewBadRequestError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusBadRequest,
+		ErrError:  BadRequest.Error(),
+		ErrCauses: causes,
+	}
+}
+
+func NewNotFoundError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusNotFound,
+		ErrError:  NotFound.Error(),
+		ErrCauses: causes,
+	}
+}
+
+func NewConflictError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusConflict,
+		ErrError:  Conflict.Error(),
+		ErrCauses: causes,
+	}
+}
+
+func NewForbiddenError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusForbidden,
+		ErrError:  Forbidden.Error(),
+		ErrCauses: causes,
+	}
+}
+
+func NewUnauthorizedError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusUnauthorized,
+		ErrError:  Unauthorized.Error(),
+		ErrCauses: causes,
+	}
+}
+
+func NewInternalServerError(causes interface{}) RestErr {
+	return RestError{
+		ErrStatus: http.StatusInternalServerError,
+		ErrError:  InternalServerError.Error(),
+		ErrCauses: causes,
+	}
+}
