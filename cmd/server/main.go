4e0263d8cce5b95efc43d6dbbcb88338b2df2611
darleet <darleet@mail.ru>
2024-05-28 10:02:13 +0300

    Updated main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/darleet/blog-graphql/internal/middleware/auth"
 	"github.com/darleet/blog-graphql/internal/ports/gql/resolver"
 	"github.com/darleet/blog-graphql/internal/ports/gql/runtime"
-	"github.com/darleet/blog-graphql/internal/repository/local"
+	"github.com/darleet/blog-graphql/internal/repository/pg"
 	"github.com/darleet/blog-graphql/internal/usecase/article"
 	"github.com/darleet/blog-graphql/internal/usecase/comment"
 	"github.com/darleet/blog-graphql/internal/usecase/vote"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -19,16 +22,25 @@ import (
 const defaultPort = "8888"
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	repo := local.NewRepository()
+	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	repo := pg.NewRepository(conn)
 
 	articles := article.NewUsecase(repo)
 	comments := comment.NewUsecase(repo)
-	votes := vote.NewUsecase(repo)
+	votes := vote.NewUsecase(nil)
 
 	res := resolver.NewRootResolvers(articles, comments, nil, votes)
 	srv := handler.NewDefaultServer(runtime.NewExecutableSchema(res))
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22
 require (
 	github.com/99designs/gqlgen v0.17.47
 	github.com/jackc/pgx/v5 v5.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/stretchr/testify v1.9.0
 	github.com/vektah/gqlparser/v2 v2.5.12
 )
@@ -18,6 +19,7 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
