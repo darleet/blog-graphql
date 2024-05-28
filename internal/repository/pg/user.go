c19d817a7a700d3b9a51cd4f27c95fb1764128fa
darleet <darleet@mail.ru>
2024-05-28 14:11:21 +0300

    Fixed index error in Loader

--- internal/repository/pg/user.go
+++ internal/repository/pg/user.go
@@ -5,23 +5,26 @@ import (
 	"github.com/darleet/blog-graphql/internal/model"
 )
 
-func (r *Repository) GetUsers(ctx context.Context, userID []string) ([]*model.User, []error) {
+func (r *Repository) GetUsers(ctx context.Context, userIDs []string) ([]*model.User, []error) {
 	query := `SELECT id, username, avatar_url FROM users WHERE id = ANY ($1)`
 
-	rows, err := r.pool.Query(ctx, query, userID)
+	rows, err := r.pool.Query(ctx, query, userIDs)
 	if err != nil {
 		return nil, []error{err}
 	}
 	defer rows.Close()
 
-	users := make([]*model.User, 0)
-	errors := make([]error, 0)
+	users := make([]*model.User, 0, len(userIDs))
+	errors := make([]error, 0, len(userIDs))
 	for rows.Next() {
 		var user model.User
 		if err := rows.Scan(&user.ID, &user.Username, &user.AvatarURL); err != nil {
+			users = append(users, nil)
 			errors = append(errors, err)
+		} else {
+			users = append(users, &user)
+			errors = append(errors, nil)
 		}
-		users = append(users, &user)
 	}
 	return users, errors
 }
